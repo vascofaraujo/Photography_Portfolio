89e3105d412bfeaf3ade860c9d46d83f0ee087cf
vascofaraujo <vasco.faden.araujo@gmail.com>
2021-04-07 15:39:35 +0100

    navbar changes

--- js/main.js
+++ js/main.js
@@ -27,19 +27,11 @@ $(document).ready(function () {
 
     let nav_offset_top = $('.header_area').height() + 50;
 
-    function navbarFixed() {
-        if ($('.header_area').length) {
-            $(window).scroll(function () {
-                let scroll = $(window).scrollTop();
-                if (scroll >= nav_offset_top) {
-                    $('.header_area .main-menu').addClass('navbar_fixed');
-                } else {
-                    $('.header_area .main-menu').removeClass('navbar_fixed');
-                }
-            })
-        }
-    }
-
-    navbarFixed();
+    $(function () {
+        $(document).scroll(function () {
+          var $nav = $(".fixed-top");
+          $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        });
+      });
 
 });
